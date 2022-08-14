8e354fe1d382bca245393329e917abd7c9133059
«Elena <snogotok@gmail.com>
2022-08-14 17:49:27 +0300

    hw3

---
+++ Task_13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.WriteLine("введите координаты Xa: ");
+int Xa = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите координаты Ya: ");
+int Ya = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите координаты Za: ");
+int Za = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите координаты Xb: ");
+int Xb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите координаты Yb: ");
+int Yb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите координаты Zb: ");
+int Zb = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow((Xb - Xa), 2) + Math.Pow((Yb - Ya), 2) + Math.Pow((Zb - Za), 2));
+Console.WriteLine($"{result:f2}");
---
+++ Task_19/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 10000)
+{
+    Console.WriteLine("Вы ввели не пятизначное число");
+}
+else
+{
+    int digit1 = number / 10000;
+    int digit2 = (number / 1000) % 10;
+    int digit4 = (number / 10) % 10;
+    int digit5 = (number % 10);
+
+    if (digit1 == digit5 && digit2 == digit4)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
